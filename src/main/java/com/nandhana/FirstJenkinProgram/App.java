2a4f5162d8f2d72b90d5b9074b96509cd0a26b50
Nandhana18 <nandhanabaskar2000@gmail.com>
2023-01-23 01:20:08 -0800

    commit changes

---
+++ src/main/java/com/nandhana/FirstJenkinProgram/App.java
@@ -0,0 +1,13 @@
+package com.nandhana.FirstJenkinProgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
